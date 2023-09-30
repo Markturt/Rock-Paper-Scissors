9600348d5952e482eded9424e6b218aa15e88785
Praise <praisem52@gmail.com>
2023-09-29 16:18:29 -1100

    Game done with

--- Game.js
+++ Game.js
@@ -2,19 +2,19 @@ let playOption=["rock","paper","scissors"];
 
 function getComputerChoice(){
     let index=Math.floor(Math.random()*3);
-    compChoice=playOption[index];
+    let compChoice=playOption[index];
     return compChoice;
 }
 
 function playRound(playerSelection,computerSelection){
-    let playerChoice=playerSelection.toSmallCase();  //Converts the players input to small case
+    let playerChoice=playerSelection.toLowerCase();  //Converts the players input to small case
     if((playerChoice==="rock" && computerSelection==="scissors")||(playerChoice==="paper" && computerSelection==="rock")||(playerChoice==="scissors" && computerSelection==="paper")){
         return `You Win ${playerSelection} beats ${computerSelection}`;
     }
-    else if((computerSelection==="rock" && playerChoice==="scissors")||(computerSelection="paper" && playerChoice==="rock")|| (computerSelection==="scissors" && playerChoice==="paper")){
+    else if((computerSelection==="rock" && playerChoice==="scissors")||(computerSelection==="paper" && playerChoice==="rock")|| (computerSelection==="scissors" && playerChoice==="paper")){
         return`You Lose ${computerSelection} beats ${playerSelection}`;
     }
-    else if(computerSelection===){
+    else if(computerSelection===playerChoice){
         return "Round tied";
     }
     else{
